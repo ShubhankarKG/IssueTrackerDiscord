119811b80d4bc77cea3287d126aac10b217cd293
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-03-23 12:45:34 +0530

    basic discord bot setup

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const Discord = require("discord.js");
+
 require("dotenv").config();
+
+const client = new Discord.Client();
+let channel;
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+client.on('ready', async () => {
+    channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_NUMBER);
+    console.log("Bot is ready");
+})
+
+client.on('message', (msg) => {
+    if (msg.content === "Hello") {
+        msg.reply("Hi!")
+    }
+});
+
+client.on("githubMessage", (msg) => {
+    channel.send(`New issue created: ${msg.issue.html_url}`);
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.post("/github", async (req, res) => {
+    client.emit("githubMessage", req.body);
+    res.status(200).json({ result: "success" });
+})
 
 const port = process.eventNames.PORT || 8000;
 app.listen(port, () => {
