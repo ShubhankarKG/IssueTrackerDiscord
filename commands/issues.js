253a7377199efa1dd3ce4743b1b5211c727cbcb0
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-03-25 19:04:33 +0530

    issues api

--- commands/issues.js
+++ commands/issues.js
@@ -1,11 +1,24 @@
+const axios = require("axios");
+
 module.exports = {
     name: 'issues',
-    description: "Shows the issues of a repo",
+    description: "Shows the open issues of a repo",
     usage: 'issues <repo_owner_name> <repo_name>',
     args: true,
-    execute(message, args){
+    async execute(message, args) {
         owner = args[0];
         repo = args[1];
-        message.channel.send(`This will show all issues from ${repo} owned by ${owner}`);
+        const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/issues`,
+            {
+                headers: {
+                    Accept: "application/vnd.github.v3+json"
+                }
+            }
+        )
+        const data = response.data.filter((issue) => !(issue.hasOwnProperty("pull_request")))
+        message.channel.send(`Okay, showing all open issues from ${repo} owned by ${owner}`);
+        for (let i = 0; i < data.length; i += 1) {
+            message.channel.send(data[i].html_url);
+        }
     }
 }
